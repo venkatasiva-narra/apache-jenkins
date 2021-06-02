5452b373ac9c7d0af984c47e9680d35477702d4f
venkatasiva-narra <75665980+venkatasiva-narra@users.noreply.github.com>
2021-06-02 19:51:41 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -1,9 +1,19 @@
 resource "aws_subnet" "test_public_subnet" {
-  vpc_id            = "${aws_vpc.test_vpc.id}"
+  vpc_id            = aws_vpc.test_vpc.id
   cidr_block        = "192.168.0.0/24"
   availability_zone = "us-west-2a"
 
   tags = {
     Name = "test_public_subnet"
+  }
+}
+
+resource "aws_subnet" "test_public_subnet_1" {
+  vpc_id            = aws_vpc.test_vpc.id
+  cidr_block        = "192.168.2.0/24"
+  availability_zone = "us-west-2b"
+
+  tags = {
+    Name = "test_public_subnet_1"
   }
 }
