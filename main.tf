cc6a60038b38e09b1888f3043a2ccc65ba01fefe
venkatasiva-narra <75665980+venkatasiva-narra@users.noreply.github.com>
2021-06-02 15:54:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,5 @@
 
 
-resource "aws_subnet" "test_public_subnet" {
-  vpc_id            = "${aws_vpc.test_vpc.id}"
-  cidr_block        = "192.168.0.0/24"
-  availability_zone = "us-west-2a"
-
-  tags = {
-    Name = "test_public_subnet"
-  }
-}
 
 resource "aws_route_table" "test_route" {
   vpc_id = "${aws_vpc.test_vpc.id}"
