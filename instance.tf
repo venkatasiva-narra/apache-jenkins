b140a8de160b46fa869b054f83270221d48b7bee
venkatasiva-narra <75665980+venkatasiva-narra@users.noreply.github.com>
2021-06-02 18:32:19 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -2,18 +2,11 @@ resource "aws_instance" "test_instance" {
     ami               = "ami-0cf6f5c8a62fa5da6"
     instance_type     = "t2.micro"
     availability_zone = "us-west-2a"
+    associate_public_ip_address = true
     subnet_id         = "${aws_subnet.test_public_subnet.id}"
-    security_groups   = ["${aws_security_group.test_security.name}"]
+    vpc_security_group_ids  = ["${aws_security_group.test_security.id}"]
     key_name          = "apache-webserver"
-    user_data         = << EOF
-                    #!/bin/bash
-                    yum update -y
-                    yum install httpd -y
-                    service httpd start
-                    cd /var/www/html/
-                    echo "apache web" >index.html
-                    chkconfig httpd on
-                    EOF
+    user_data         = file("./apace.sh")
 
     tags = {
         Name = "test_instance"
