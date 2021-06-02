c861443c3f24f57c4cc6bd2150da4e3f980e8873
venkatasiva-narra <75665980+venkatasiva-narra@users.noreply.github.com>
2021-06-02 15:55:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,6 @@
 
 
 
-resource "aws_route_table" "test_route" {
-  vpc_id = "${aws_vpc.test_vpc.id}"
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.test_ig.id}"
-  }
-}
-
-resource "aws_route_table_association" "test_public" {
-  subnet_id      = "${aws_subnet.test_public_subnet.id}"
-  route_table_id = "${aws_route_table.test_route.id}"
-}
 
 
 resource "aws_security_group" "test_security" {
