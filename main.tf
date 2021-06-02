b201fcd4f53a454c81e07ccbd8e8e68ab0765691
venkatasiva-narra <75665980+venkatasiva-narra@users.noreply.github.com>
2021-06-02 15:50:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,19 +53,19 @@ resource "aws_security_group" "test_security" {
         from_port = 22
         to_port   = 22
         protocol  = "tcp"
-        cidr_block = ["0.0.0.0/0"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
     ingress {
         from_port = 80
         to_port   = 80
         protocol  = "tcp"
-        cidr_block = ["0.0.0.0/0"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
     egress {
         from_port = 0
         to_port   = 0
         protocol  = "-1"
-        cidr_block = ["0.0.0.0/0"]
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
 
