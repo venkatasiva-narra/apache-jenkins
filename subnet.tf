4018d261082b6db2fde71559f9a27da35b24de8a
venkatasiva-narra <75665980+venkatasiva-narra@users.noreply.github.com>
2025-05-10 01:05:00 -0400

    Update subnet.tf
    east

--- subnet.tf
+++ subnet.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "test_public_subnet" {
   vpc_id            = aws_vpc.test_vpc.id
   cidr_block        = "192.168.0.0/24"
-  availability_zone = "us-west-2a"
+  availability_zone = "us-east-2a"
 
   tags = {
     Name = "test_public_subnet"
@@ -11,7 +11,7 @@ resource "aws_subnet" "test_public_subnet" {
 resource "aws_subnet" "test_public_subnet_1" {
   vpc_id            = aws_vpc.test_vpc.id
   cidr_block        = "192.168.2.0/24"
-  availability_zone = "us-west-2b"
+  availability_zone = "us-east-2b"
 
   tags = {
     Name = "test_public_subnet_1"
