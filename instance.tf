773292c99b2c39a248d26580233e75a324a8f2c8
venkatasiva-narra <75665980+venkatasiva-narra@users.noreply.github.com>
2025-05-10 01:05:35 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "test_instance" {
     ami               = "ami-0cf6f5c8a62fa5da6"
     instance_type     = "t2.micro"
-    availability_zone = "us-west-2a"
+    availability_zone = "us-east-2a"
     associate_public_ip_address = true
     subnet_id         = "${aws_subnet.test_public_subnet.id}"
     vpc_security_group_ids  = ["${aws_security_group.test_security.id}"]
